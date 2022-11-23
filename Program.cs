123ebdb51e1736dcce434ec80afa084378a1dc9c
azaza <zador94@mail.ru>
2022-11-23 18:28:10 +0300

    commit monitor

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+[DllImport("gdi32.dll")]
+static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+using (Graphics g = Graphics.FromHwnd(IntPtr.Zero))
+{ 
+        IntPtr hdc = g.GetHdc();
+        int resultW = GetDeviceCaps(hdc, 4); //Длина экрана
+        int resultH = GetDeviceCaps(hdc, 6); //Ширина экрана
+        g.ReleaseHdc();
+        int resultDiag = (int)((Math.Sqrt(Math.Pow(resultW, 2) + Math.Pow(resultH, 2)))/ 25.4);
+        Console.WriteLine($"Диагональ монитора: {resultDiag} дюйма");
+}
+
+
